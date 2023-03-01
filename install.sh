a349233b0a2223ebdb1d9e434fd69f124f27fa97
mAmineChniti <emin.chniti@esprit.tn>
2023-03-01 18:21:47 +0100

    final instaal/makefile

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
-# Install SDL1.2 and other dependencies
-sudo apt-get update && sudo apt-get install libsdl1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libpulse-dev libxt-dev gcc make mingw-w64-i686-dev mingw-w64 build-essential gcc-multilib -y
+#!/bin/bash
+
+sudo apt-get update \
+&& sudo apt-get install libsdl1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev -y \
+&& sudo apt-get install gcc make build-essential gcc-multilib -y
